866df52e2e2ecb31926276fb7e3d908ce82fc9b0
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-25 09:13:49 +1100

    Ignore the gstreamer generated files.

--- from-gstreamer.py
+++ from-gstreamer.py
@@ -50,7 +50,7 @@ while True:
         videotestsrc name=src pattern={pattern} num-buffers=1 !
     	video/x-raw,format=RGB,width={width},height={height},framerate=25/1,pixel-aspect-ratio=1/1 !
         pngenc !
-        multifilesink location=input/{pattern}.png sync=false
+        multifilesink location=input/gst-{pattern}.png sync=false
         """.format(
             pattern=current_pattern.value_nick,
             width=1280,
@@ -63,35 +63,3 @@ while True:
 
     i += 1
     src.set_property("pattern", i)
-
-
-#for infile in os.listdir(
-
-
-#from IPython import embed; embed()
-
-"""
-(0): smpte            - SMPTE 100% color bars
-(1): snow             - Random (television snow)
-(2): black            - 100% Black
-(3): white            - 100% White
-(4): red              - Red
-(5): green            - Green
-(6): blue             - Blue
-(7): checkers-1       - Checkers 1px
-(8): checkers-2       - Checkers 2px
-(9): checkers-4       - Checkers 4px
-(10): checkers-8       - Checkers 8px
-(11): circular         - Circular
-(12): blink            - Blink
-(13): smpte75          - SMPTE 75% color bars
-(14): zone-plate       - Zone plate
-(15): gamut            - Gamut checkers
-(16): chroma-zone-plate - Chroma zone plate
-(17): solid-color      - Solid color
-(18): ball             - Moving ball
-(19): smpte100         - SMPTE 100% color bars
-(20): bar              - Bar
-(21): pinwheel         - Pinwheel
-(22): spokes           - Spokes
-"""
