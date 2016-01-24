181f98b24db815875df3e48a575a660665245dac
Tim 'mithro' Ansell <mithro@mithis.com>
2016-01-25 10:07:24 +1100

    Adding a tool for visually checking the vsync rate.

---
+++ dynamic/vsync-color/vsync-color.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 et sts=4 ai:
+
+import sys, gi, signal
+
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject
+
+GObject.threads_init()
+Gst.init([])
+
+def on_error(bus, message):
+    (error, debug) = message.parse_error()
+    print('Error-Details: #%u: %s' % (error.code, debug))
+    sys.exit(1)
+
+p = """
+    multifilesrc location="vsync%04d.png" index=0 loop=1 caps="image/png,framerate=\(fraction\){framerate}" !
+    pngdec !
+    videoscale !
+    video/x-raw,width={width},height={height},pixel-aspect-ratio=1/1 !
+    videoconvert !
+    xvimagesink double-buffer=1
+""".format(
+    framerate=sys.argv[1],
+    width=1280,
+    height=720,
+    )
+print(p)
+
+pipe = Gst.parse_launch(p)
+pipe.bus.add_signal_watch()
+pipe.bus.connect("message::error", on_error)
+pipe.set_state(Gst.State.PLAYING)
+
+mainloop = GObject.MainLoop()
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+mainloop.run()
